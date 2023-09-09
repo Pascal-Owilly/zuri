c54f60fbf85540477b871bf70b06b1e5ec819125
Pascal-Owilly <pascalouma54@gmail.com>
2023-09-09 17:02:37 +0300

    add custon status code

--- info/views.py
+++ info/views.py
@@ -1,20 +1,18 @@
-from django.http import JsonResponse
+from django.http import HttpResponse
+import json
 from datetime import datetime
 import pytz
 
 def get_info(request):
-    slack_name = request.GET.get('slack_name')
-    track = request.GET.get('track')
-
     # Get the current day of the week
     current_day = datetime.now(pytz.utc).strftime('%A')
 
-    # Get the current UTC time within a +/-2 minute window
+    # Get the current UTC time within a +/-2 minute 
     utc_time = datetime.now(pytz.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
 
     # GitHub file and repository URLs
-    github_file_url = "https://github.com/username/repo/blob/main/file_name.ext"
-    github_repo_url = "https://github.com/username/repo"
+    github_file_url = "https://github.com/Pascal-Owilly/zuri-training.git"
+    github_repo_url = "https://github.com/Pascal-Owilly/zuri-training"
 
     # Create the JSON response
     response_data = {
@@ -27,4 +25,8 @@ def get_info(request):
         "status_code": 200
     }
 
-    return JsonResponse(response_data)
+    # HttpResponse with a custom status message
+    response = HttpResponse(json.dumps(response_data), content_type='application/json', status=200)
+    response.reason_phrase = 'OK'
+
+    return response
