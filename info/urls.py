5f1cb6b09e64e3240bb137aa037f6450aa612a48
Pascal-Owilly <pascalouma54@gmail.com>
2023-09-12 10:00:13 +0300

    add DOCUMENTATION.md

--- info/urls.py
+++ info/urls.py
@@ -9,9 +9,9 @@ from .views import PersonListCreateView, PersonDetailView
 
 urlpatterns = [ 
     # task 1
-    path('', views.api_get_example_slack_info, name='api_get_example_slack_info'),
+    # path('', views.api_get_example_slack_info, name='api_get_example_slack_info'),
 
     # task 2
-    path('persons/', PersonListCreateView.as_view()),
-    path('persons/<int:pk>/', PersonDetailView.as_view()),
+    path('', PersonListCreateView.as_view()),
+    path('<int:pk>/', PersonDetailView.as_view()),
 ]
