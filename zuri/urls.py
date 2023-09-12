ab46ffa82cae8b86806d56735a53eb2b0c185e9b
Pascal-Owilly <pascalouma54@gmail.com>
2023-09-12 11:09:07 +0300

    add requirements

--- zuri/urls.py
+++ zuri/urls.py
@@ -24,7 +24,7 @@ from rest_framework import permissions
 schema_view = get_schema_view(
     openapi.Info(
         title="A simple API which performs CRUD operations on persons",
-        
+
         default_version='v1',
         # description="Th API",
         terms_of_service="https://www.example.com/terms/",
@@ -39,7 +39,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('info.urls')),
 
-    # swaggerdocumentation
+    # swagger documentation
 
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
