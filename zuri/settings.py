a825da7fd9fe2e13f8bce18d303050b0d3f429b3
Pascal-Owilly <pascalouma54@gmail.com>
2023-09-12 12:06:31 +0300

    secure secret key and debug in .env

--- zuri/settings.py
+++ zuri/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ckc*unjq^-tq#d^al^%!$9h!a43^4ciu+h^7f9hay*q#9smx6t'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True    
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['http://owilly.pythonanywhere.com/', 'http://enceptics.pythonanywhere.com/', 'pythonaywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -43,7 +46,6 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'drf_yasg',
-
 
 
     # local
